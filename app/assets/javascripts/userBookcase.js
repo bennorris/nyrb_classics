6394c803403118c02dfe81ea8d6b445591f139cd
github username <github email address>
2017-02-02 19:29:19 -0500

    cleaned up show page. fixed styling on bookcase more info.

--- app/assets/javascripts/userBookcase.js
+++ app/assets/javascripts/userBookcase.js
@@ -1,12 +1,12 @@
 var appendBooks = function() {
 
-$(document).on('click', '.all-the-books a', function(e) {
+$(document).on('click', '.all-the-books .more-book-info', function(e) {
   e.preventDefault();
   var id = $(this).attr('id');
 
   if ( $(`#img-placeholder-${id}`).hasClass('clicked') ) {
     $(`#img-placeholder-${id} img`).show();
-    $(`#img-placeholder-${id} p`).hide();
+    $(`#img-placeholder-${id} p, h2, h3`).hide();
     $(`#${id}`).text('more information');
     // $(`#img-placeholder-${id} a`).text('more information');
     $(`#img-placeholder-${id}`).removeClass('clicked');
@@ -15,7 +15,7 @@ $(document).on('click', '.all-the-books a', function(e) {
   $.getJSON( `/books/${id}.json`, function(res) {
       $(`#img-placeholder-${id}`).addClass('fixed-img-div');
       $(`#img-placeholder-${id} img`).hide();
-      $(`#img-placeholder-${id}`).append(`<p>${res.title}<br><br>by ${res.author}<br><br>${res.description}</p>`);
+      $(`#img-placeholder-${id}`).append(`<h2>${res.title}</h2><h3>by ${res.author}</h3><p><a class='nyrb-link' href='${res.link}' target="_blank">on nyrb.com</a><br><br>${res.description}</p>`);
       $(`#img-placeholder-${id}`).addClass('clicked');
       $(`#${id}`).text('back');
     });
